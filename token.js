4689348640ea754086a0e6b126e75ad6177fa19b
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-13 17:08:21 +0700

    Update token.js

--- token.js
+++ token.js
@@ -95,12 +95,12 @@ setTimeout(function(){
        
       
     }, 2000);
-     $("#notif").text("- Kode E-Commerce sudah kadaluarsa -");
+     $("#notif").text("- Kode Virtual sudah kadaluarsa -");
      $("#notif").css("color", "red");
         }
     });
       setTimeout(() => {
-     document.getElementById("notif").innerHTML = "Untuk mendapatkan kode E-Commerce,<br/>Silahkan klik tombol dibawah ini";
+     document.getElementById("notif").innerHTML = "Untuk mendapatkan kode Virtual,<br/>Silahkan klik tombol dibawah ini";
      $("#djload").hide();  
    $("#notif").css("color", "black");
  $("#sixpin").val("");
